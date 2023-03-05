1616f64fa5a1252f1b0ae96dd5cde99c59760889
Jitish Kolanjery <jitish.kolanjery@getcruise.com>
2023-03-04 22:27:43 -0800

    flask: bare bone flask webserver

--- Flask_RESTful/flask_api_server.py
+++ Flask_RESTful/flask_api_server.py
@@ -11,9 +11,6 @@ Built and tested with Python 3.7 on Raspberry Pi 4 Model B
 import logging
 from flask import Flask, request, render_template                                    # (1)
 from flask_restful import Resource, Api, reqparse, inputs                            # (2)
-from gpiozero import PWMLED, Device                                                  # (3)
-from gpiozero.pins.pigpio import PiGPIOFactory
-
 
 # Initialize Logging
 logging.basicConfig(level=logging.WARNING)  # Global logging configuration
@@ -21,30 +18,9 @@ logger = logging.getLogger('main')  # Logger for this module
 logger.setLevel(logging.INFO) # Debugging for this file.
 
 
-# Initialize GPIOZero
-Device.pin_factory = PiGPIOFactory() #set GPIOZero to use PiGPIO by default
-
 # Flask & Flask-RESTful instance variables
 app = Flask(__name__) # Core Flask app.                                              # (4)
 api = Api(app) # Flask-RESTful extension wrapper                                     # (5)
-
-
-# Global variables
-LED_GPIO_PIN = 21
-led = None # PWMLED Instance. See init_led()
-state = {                                                                            # (6)
-    'level': 50 # % brightless of LED.
-}
-
-"""
-GPIO Related Functions
-"""
-def init_led():
-    """Create and initialise an PWMLED Object"""
-    global led
-    led = PWMLED(LED_GPIO_PIN)
-    led.value = state['level'] / 100                                                 # (7)
-
 
 """
 Flask & Flask-Restful Related Functions
@@ -56,48 +32,7 @@ Flask & Flask-Restful Related Functions
 def index():
     """Make sure inde.html is in the templates folder
     relative to this Python file."""
-    return render_template('index_api_client.html', pin=LED_GPIO_PIN)                # (9)
-
-
-# Flask-restful resource definitions.
-# A 'resource' is modeled as a Python Class.
-class LEDControl(Resource):  # (10)
-
-    def __init__(self):
-        self.args_parser = reqparse.RequestParser()                                  # (11)
-
-        self.args_parser.add_argument(
-            name='level',  # Name of arguement
-            required=True,  # Mandatory arguement
-            type=inputs.int_range(0, 100),  # Allowed range 0..100                   # (12)
-            help='Set LED brightness level {error_msg}',
-            default=None)
-
-
-    def get(self):
-        """ Handles HTTP GET requests to return current LED state."""
-        return state  # (13)
-
-
-    def post(self):
-        """Handles HTTP POST requests to set LED brightness level."""
-        global state                                                                 # (14)
-
-        args = self.args_parser.parse_args()                                         # (15)
-
-        # Set PWM duty cycle to adjust brightness level.
-        state['level'] = args.level                                                  # (16)
-        led.value = state['level'] / 100                                             # (17)
-        logger.info("LED brightness level is " + str(state['level']))
-
-        return state                                                                 # (18)
-
-
-# Initialise Module.
-init_led()
-# Register Flask-RESTful resource and mount to server end point /led
-api.add_resource(LEDControl, '/led')                                                 # (19)
-
+    return render_template('index_api_client.html') #, pin=LED_GPIO_PIN)                # (9)
 
 if __name__ == '__main__':
 
